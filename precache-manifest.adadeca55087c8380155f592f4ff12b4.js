d9339f2cad587f7535c20e149188ba1ea1f99e20
whtalx <whtalx@gmail.com>
2019-11-20 21:50:50 +0300

    Updates

---
+++ precache-manifest.adadeca55087c8380155f592f4ff12b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7870e0cd3a4acd1cd7fd5964d894d5f",
+    "url": "/battleships/index.html"
+  },
+  {
+    "revision": "527ade6cddea2b9b9fe3",
+    "url": "/battleships/static/css/main.9e6f2c4a.chunk.css"
+  },
+  {
+    "revision": "637ee7fb08ef850c10b0",
+    "url": "/battleships/static/js/2.5fd2144b.chunk.js"
+  },
+  {
+    "revision": "527ade6cddea2b9b9fe3",
+    "url": "/battleships/static/js/main.ae510cf1.chunk.js"
+  },
+  {
+    "revision": "dda6aeacf758f0c33f45",
+    "url": "/battleships/static/js/runtime-main.c903405c.js"
+  }
+]);
