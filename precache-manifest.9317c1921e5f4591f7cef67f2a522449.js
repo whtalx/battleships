f051bfd159186426e132f62c11a4f78aa610b68e
alexander-b <alexander.b@ramotion.agency>
2020-01-22 13:01:05 +0300

    Updates

---
+++ precache-manifest.9317c1921e5f4591f7cef67f2a522449.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce38535cc081c9cfc097cc22caafbf2c",
+    "url": "/battleships/index.html"
+  },
+  {
+    "revision": "ba3e8b0b0b73a7e13080",
+    "url": "/battleships/static/css/main.48d00986.chunk.css"
+  },
+  {
+    "revision": "48c248408ed5f67f925c",
+    "url": "/battleships/static/js/2.74946fef.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/battleships/static/js/2.74946fef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba3e8b0b0b73a7e13080",
+    "url": "/battleships/static/js/main.6dbd1ec2.chunk.js"
+  },
+  {
+    "revision": "88573150d090917c0eb0",
+    "url": "/battleships/static/js/runtime-main.140582b2.js"
+  },
+  {
+    "revision": "2c2d898c2e9b0eb0e4273c950dc5eab0",
+    "url": "/battleships/static/media/DOSVGA.2c2d898c.woff2"
+  },
+  {
+    "revision": "4e84da54b8e6d390ca958b9389f081a4",
+    "url": "/battleships/static/media/DOSVGA.4e84da54.woff"
+  },
+  {
+    "revision": "589470abff3d7856955b8bb3250fd984",
+    "url": "/battleships/static/media/DOSVGA.589470ab.eot"
+  },
+  {
+    "revision": "bf8e3c969b600a1f2f8946affa0442b4",
+    "url": "/battleships/static/media/DOSVGA.bf8e3c96.ttf"
+  },
+  {
+    "revision": "bfbf193aa44677b39b21373e3edbdf33",
+    "url": "/battleships/static/media/DOSVGA.bfbf193a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/battleships/precache-manifest.e64ca105b43455a2e05fb95989e1c4c2.js"
+  "/battleships/precache-manifest.9317c1921e5f4591f7cef67f2a522449.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/battleships/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
